7f04f89cc63cb20d74b19cda23c02e86c5adeb21
Woonggi Min <min20000416@naver.com>
2022-02-21 14:16:34 +0900

    [22.02.21][민웅기] DB 저장 및 정보 접근 추가

--- accountapp/views.py
+++ accountapp/views.py
@@ -1,8 +1,22 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
 from django.shortcuts import render
+from accountapp.models import HelloWorld
 
 # Create your views here.
 
 def hello_world(request):
-    # return HttpResponse('Hello world!')
-    return render(request, 'accountapp/hello_world.html')
+    if request.method == "POST":
+        temp = request.POST.get('hello_world_input')
+
+        new_hello_world = HelloWorld()
+        new_hello_world.text = temp
+        new_hello_world.save()
+
+        hello_world_list = HelloWorld.objects.all()
+
+        # return render(request, 'accountapp/hello_world.html', context={'hello_world_list': hello_world_list})
+        return HttpResponseRedirect(reverse('accountapp:hello_world'))
+    else:
+        hello_world_list = HelloWorld.objects.all()
+        return render(request, 'accountapp/hello_world.html', context={'hello_world_list': hello_world_list})

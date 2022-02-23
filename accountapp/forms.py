09620572289580f2e9b5a6d3d5e7b27c6c3d6d1d
Woonggi Min <min20000416@naver.com>
2022-02-24 02:20:06 +0900

    [22.02.24][민웅기] error fix

--- accountapp/forms.py
+++ accountapp/forms.py
@@ -5,3 +5,4 @@ class AccountUpdateForm(UserCreationForm):
         super().__init__(*args, **kwargs)
 
         self.fields['username'].disabled = True # input 비활성화 및 value 값을 바꿔서 보내도 서버에서 받지 않음
+
--- accountapp/views.py
+++ accountapp/views.py
@@ -1,6 +1,6 @@
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
-from django.views.generic import CreateView, DetailView, UpdateView
+from django.views.generic import CreateView, DetailView, UpdateView, DeleteView
 from django.http import HttpResponse, HttpResponseRedirect
 from django.urls import reverse, reverse_lazy
 from django.shortcuts import render
@@ -34,11 +34,19 @@ class AccountCreateView(CreateView):
 
 class AccountDetailView(DetailView):
     model = User
+    context_object_name = 'target_user'
     template_name = 'accountapp/detail.html'
     context_object_name = 'target_user' # template에서 사용하는 user 객체 이름을 설정(다른 사람이 왔을 때도 정보를 보여줄 수 있게)
 
 class AccountUpdateView(UpdateView):
     model = User
+    context_object_name = 'target_user'
     form_class = AccountUpdateForm
     success_url = reverse_lazy('accountapp:hello_world')
     template_name = 'accountapp/update.html'
+
+class AccountDeleteView(DeleteView):
+    model = User
+    context_object_name = 'target_user'
+    success_url = reverse_lazy('accountapp:login')
+    template_name = 'accountapp/delete.html'

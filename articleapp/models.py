51ac8fbba0dcfef58395b387923fd361c4863d40
Woonggi Min <min20000416@naver.com>
2022-02-26 11:03:14 +0900

    [22.02.26][민웅기] dev branch 생성

--- articleapp/models.py
+++ articleapp/models.py
@@ -1,3 +1,14 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 # Create your models here.
+
+class Article(models.Model):
+    writer = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, related_name='article') # 작성자
+
+    title = models.CharField(max_length=200, null=True) # 제목
+    image = models.ImageField(upload_to='article/', null=False) # 이미지
+    content = models.TextField(null=True) # 내용
+
+    created_at = models.DateField(auto_now_add=True, null=True) # 작성일시
+
